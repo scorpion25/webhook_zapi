67f65b21d2708152c4d4f0ca3f82d813573763e9
Anderson Silva <s.ct@live.com>
2025-05-18 22:21:42 -0300

    Ativa logs para debug no webhook

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+# Dados reais
+ZAPI_URL = "https://api.z-api.io/instances/3E1664822BF440DCF6C9FE99C2B48794/token/3E6D6FDD5AF8252B380DACA8/send-text"
+CLIENT_TOKEN = "F466390c69345429ba80cec680a7f5987S"
+SUPORTE_NUMERO = "5519993203350"
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "Client-Token": CLIENT_TOKEN
+}
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
@@ -22,3 +38,8 @@ def webhook():
         print("✅ Retorno da Z-API:", r.status_code, r.text)
 
     return jsonify({"status": "ok"})
+
+# Necessário para a Render
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
